82e1e8d688aac9526ab0990437be755aaefefc81
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-11-14 16:36:51 +0000

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          '50': '#edfcf5',
+          '100': '#d3f8e5',
+          '200': '#abefd0',
+          '300': '#74e1b6',
+          '400': '#3ecc98',
+          '500': '#18b17e',
+          '600': '#0c8f66',
+          '700': '#0a7255',
+          '800': '#0a5b44',
+          '900': '#094b39',
+          '950': '#042a21',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
